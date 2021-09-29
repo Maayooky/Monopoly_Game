3d3a0604cea9879db54983c3e56e2d4e19716781
Maayooky <zilber.maayan@gmail.com>
2021-09-29 11:21:08 +0300

    Test_start
    printing doesn't seem to be visible in the godot editor when running the debuger
    
    when succesful printing is achived the test class&scene will start prooving themselves usefull

---
+++ Test1.gd
@@ -0,0 +1,11 @@
+extends MonopolyResource
+class_name Test1 
+var board = null
+var save_name = "g1" 
+
+func _ready():
+	#board = GameBoard.new()
+	#board.load_from_saved(save_name)
+	#var context = board.context
+	#print_line(context.tile)
+	print("hello")
